82b2b106d5c4057d2cff54d2352e21f19eeb72a3
dpu <johnson0917@gmail.com>
2016-02-20 17:17:51 -0800

    initial commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/dongqiuyepu/Sites/supplywiz/catalog/');
+define('DIR_SYSTEM', '/Users/dongqiuyepu/Sites/supplywiz/system/');
+define('DIR_LANGUAGE', '/Users/dongqiuyepu/Sites/supplywiz/catalog/language/');
+define('DIR_TEMPLATE', '/Users/dongqiuyepu/Sites/supplywiz/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/dongqiuyepu/Sites/supplywiz/system/config/');
+define('DIR_IMAGE', '/Users/dongqiuyepu/Sites/supplywiz/image/');
+define('DIR_CACHE', '/Users/dongqiuyepu/Sites/supplywiz/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/dongqiuyepu/Sites/supplywiz/system/storage/download/');
+define('DIR_LOGS', '/Users/dongqiuyepu/Sites/supplywiz/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/dongqiuyepu/Sites/supplywiz/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/dongqiuyepu/Sites/supplywiz/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'johnson687');
+define('DB_DATABASE', 'supplywiz');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
